a3cff0303dd8cf0f9d2ade164bce4406542943ba
Maria.Pelageycheva <m.v.pelageycheva@urfu.ru>
2024-11-21 10:49:39 +0500

    firebase settings

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,19 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 
 import 'common/app.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main()async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+);
+final notificationSettings = await FirebaseMessaging.instance.requestPermission(provisional: true);
+
+final apnsToken = await FirebaseMessaging.instance.getAPNSToken();
+if (apnsToken != null) {
+  print(apnsToken);
+}
   runApp(const App());
 }
