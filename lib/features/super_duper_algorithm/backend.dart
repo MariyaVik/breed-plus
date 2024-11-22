150e2617825673e1acf6592a4203455e937727a2
Oleg "OSA413" Sokolov <OSA413@users.noreply.github.com>
2024-11-22 23:37:57 +0500

    xtrcyugihop

---
+++ lib/features/super_duper_algorithm/backend.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/widgets.dart';
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+
+class Backend {}
--- lib/features/super_duper_algorithm/index.dart
+++ lib/features/super_duper_algorithm/index.dart
@@ -1,11 +1,23 @@
 import 'dart:async';
-
-import 'package:flutter/widgets.dart';
-import 'package:path/path.dart';
-import 'package:sqflite/sqflite.dart';
+import "package:excel/excel.dart";
 
 class API {
-  static Future loadXls() {
+  static Future loadXls(List<int> bytes) {
+    var excel = Excel.decodeBytes(bytes);
+    for (var table in excel.tables.keys) {
+        print(table); //sheet Name
+        print(excel.tables[table].maxColumns);
+        print(excel.tables[table].maxRows);
+        for (var row in excel.tables[table].rows) {
+            for (var cell in row) {
+                print('cell ${cell.rowIndex}/${cell.columnIndex}');
+                final value = cell.value;
+                final numFormat = cell.cellStyle?.numberFormat ?? NumFormat.standard_0;
+                switch(value){
+                }
+            }
+        }
+
     throw Future.error(Error());
   }
 
