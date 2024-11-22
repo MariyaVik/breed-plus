19ca5911f898948886d357013704fbd43cc8dd1e
Oleg "OSA413" Sokolov <OSA413@users.noreply.github.com>
2024-11-22 23:26:36 +0500

    Добавил набросок api

---
+++ lib/features/super_duper_algorithm/index.dart
@@ -0,0 +1,35 @@
+import 'dart:async';
+
+import 'package:flutter/widgets.dart';
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+
+class API {
+  static void loadXls() {
+    throw Error();
+  }
+
+  static void loadXlsx() {
+    throw Error();
+  }
+
+  static void loadTSV() {
+    throw Error();
+  }
+
+  static void loadCFV() {
+    throw Error();
+  }
+
+  static void matchFemale() {
+    throw Error();
+  }
+
+  static void matchMale() {
+    throw Error();
+  }
+
+  static void matchAll() {
+    throw Error();
+  }
+}
