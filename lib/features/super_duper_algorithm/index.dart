1273f51126b1927e46f63c19635dd561d6a57bef
Oleg "OSA413" Sokolov <OSA413@users.noreply.github.com>
2024-11-23 00:11:36 +0500

    Чтение и валидация заголовка таблицы

--- lib/features/super_duper_algorithm/index.dart
+++ lib/features/super_duper_algorithm/index.dart
@@ -1,22 +1,85 @@
+// ignore_for_file: curly_braces_in_flow_control_structures
+
 import 'dart:async';
 import "package:excel/excel.dart";
+
+void assertValue(String? expected, String? actual) {
+  if (expected != actual) {
+    throw Exception('Ожидал [$expected], получил [$actual]');
+  }
+}
+
+List<String?> readTableRow(List<Data?> row) {
+  final result = [] as List<String?>;
+  for (var cell in row) {
+    if (cell == null)
+      result.add(null);
+    else
+      result.add(getCellValue(cell));
+  }
+  return result;
+}
+
+String? getCellValue(Data cell) {
+  final value = cell.value;
+  switch (value) {
+    case null:
+      return null;
+    case TextCellValue():
+      return value.value.toString();
+    case FormulaCellValue():
+      return value.formula;
+    case IntCellValue():
+      return value.value.toString();
+    case BoolCellValue():
+      return value.value.toString();
+    case DoubleCellValue():
+      return value.value.toString();
+    case DateCellValue():
+      return value.asDateTimeLocal().toString();
+    case TimeCellValue():
+      return value.asDuration().toString();
+    case DateTimeCellValue():
+      return value.asDateTimeLocal().toIso8601String();
+  }
+}
+
+List<String?> assertHeader(row) {
+  final rowData = readTableRow(row);
+  // Assert assumptions
+  if (row.length != 13) {
+    Future.error(Exception("Количество столбцов не равно 13"));
+  }
+  if (row[0]!.rowIndex == 0) {
+    assertValue(rowData[0], "ID_особи");
+    assertValue(rowData[1], "Пол");
+    assertValue(rowData[2], "Порода");
+    assertValue(rowData[3], "Дата_Рождения");
+    assertValue(rowData[4], "Родитель_папа");
+    assertValue(rowData[5], "Родитель_мама");
+    assertValue(rowData[6], "Удой л/день");
+    assertValue(rowData[7], "Упитанность");
+    assertValue(rowData[8], "Коэффициент инбридинга (F)");
+    assertValue(rowData[9], "Прирост веса кг/день");
+    assertValue(rowData[10], "Здоровье (1-10)");
+    assertValue(rowData[11], "Фертильность (%)");
+    assertValue(rowData[12], "Генетическая ценность (баллы)");
+  }
+  return rowData;
+}
+
+void packData() {}
 
 class API {
   static Future loadXls(List<int> bytes) {
     var excel = Excel.decodeBytes(bytes);
-    for (var table in excel.tables.keys) {
-        print(table); //sheet Name
-        print(excel.tables[table].maxColumns);
-        print(excel.tables[table].maxRows);
-        for (var row in excel.tables[table].rows) {
-            for (var cell in row) {
-                print('cell ${cell.rowIndex}/${cell.columnIndex}');
-                final value = cell.value;
-                final numFormat = cell.cellStyle?.numberFormat ?? NumFormat.standard_0;
-                switch(value){
-                }
-            }
-        }
+    for (var tableKey in excel.tables.keys) {
+      final table = excel.tables[tableKey];
+      if (table == null) continue;
+      for (var row in table.rows) {
+        final rowData = assertHeader(row);
+      }
+    }
 
     throw Future.error(Error());
   }
