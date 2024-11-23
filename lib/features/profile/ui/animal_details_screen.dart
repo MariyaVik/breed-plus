f3c1aaa49a901aef5d609297b14d11955a7e0e2f
Maria.Pelageycheva <m.v.pelageycheva@urfu.ru>
2024-11-23 18:04:21 +0500

    added milk

--- lib/features/profile/ui/animal_details_screen.dart
+++ lib/features/profile/ui/animal_details_screen.dart
@@ -1,14 +1,84 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
+import '../../super_duper_algorithm/index.dart';
 import '../../super_duper_algorithm/passport.dart';
 
-class AnimalDetailsScreen extends StatelessWidget {
+class AnimalDetailsScreen extends StatefulWidget {
   const AnimalDetailsScreen({
     super.key,
     required this.passport,
   });
   final Passport passport;
+
+  @override
+  State<AnimalDetailsScreen> createState() => _AnimalDetailsScreenState();
+}
+
+class _AnimalDetailsScreenState extends State<AnimalDetailsScreen> {
+  late Passport pas;
+  final TextEditingController litersCont = TextEditingController();
+  final TextEditingController daysCont = TextEditingController();
+  @override
+  void initState() {
+    super.initState();
+    pas = widget.passport;
+  }
+
+  @override
+  void dispose() {
+    litersCont.dispose();
+    daysCont.dispose();
+    super.dispose();
+  }
+
+  void getInfo() async {
+    pas = (await API.getCow(widget.passport.id)).passport;
+    setState(() {});
+  }
+
+  void _addMilkInfo(BuildContext context) {
+    showModalBottomSheet(
+      context: context,
+      builder: (_) {
+        return Container(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Center(
+                child: Text('Удой'),
+              ),
+              Text('Количество литров'),
+              TextField(
+                keyboardType: const TextInputType.numberWithOptions(),
+                controller: litersCont,
+              ),
+              Text('Количество дней'),
+              TextField(
+                keyboardType: const TextInputType.numberWithOptions(),
+                controller: daysCont,
+              ),
+              Center(
+                child: ElevatedButton(
+                  onPressed: () async {
+                    await API.setCowMilk(
+                      pas.id,
+                      int.parse(daysCont.text),
+                      double.parse(litersCont.text),
+                    );
+                    getInfo();
+                    context.pop();
+                  },
+                  child: const Text('Добавить данные'),
+                ),
+              )
+            ],
+          ),
+        );
+      },
+    );
+  }
 
   Widget _buildDetailRow(String title, String value) {
     return Padding(
@@ -30,56 +100,39 @@ class AnimalDetailsScreen extends StatelessWidget {
   }
 
   Widget _buildProfileHeader(BuildContext context) {
-    return Stack(
-      children: [
-        Center(
-          child: Card(
-            elevation: 4,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(16),
-            ),
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Column(
-                children: [
-                  CircleAvatar(
-                    radius: 40,
-                    backgroundColor: Colors.blueGrey.shade100,
-                    child: Icon(
-                      passport.gender == Gender.male
-                          ? Icons.male
-                          : Icons.female,
-                      size: 40,
-                      color: Colors.blueGrey.shade800,
-                    ),
-                  ),
-                  const SizedBox(height: 16),
-                  Text(
-                    'ID: ${passport.id}',
-                    style: const TextStyle(
-                        fontSize: 18, fontWeight: FontWeight.bold),
-                  ),
-                  const SizedBox(height: 8),
-                  Text(
-                    passport.breed,
-                    style: const TextStyle(
-                        fontSize: 16, fontStyle: FontStyle.italic),
-                  ),
-                ],
+    return Card(
+      elevation: 4,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(16),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            CircleAvatar(
+              radius: 40,
+              backgroundColor: Colors.blueGrey.shade100,
+              child: Icon(
+                widget.passport.gender == Gender.male
+                    ? Icons.male
+                    : Icons.female,
+                size: 40,
+                color: Colors.blueGrey.shade800,
               ),
             ),
-          ),
-        ),
-        Row(
-          children: [
-            IconButton(
-                onPressed: () {
-                  context.pop();
-                },
-                icon: const Icon(Icons.arrow_back_ios_new_rounded)),
+            const SizedBox(height: 16),
+            Text(
+              'ID: ${widget.passport.id}',
+              style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 8),
+            Text(
+              widget.passport.breed,
+              style: const TextStyle(fontSize: 16, fontStyle: FontStyle.italic),
+            ),
           ],
-        )
-      ],
+        ),
+      ),
     );
   }
 
@@ -100,19 +153,20 @@ class AnimalDetailsScreen extends StatelessWidget {
             ),
             const SizedBox(height: 16),
             _buildDetailRow('Дата рождения',
-                passport.bday.toLocal().toString().split(' ')[0]),
-            _buildDetailRow('Отец (ID)', passport.father.toString()),
-            _buildDetailRow('Мать (ID)', passport.mother.toString()),
+                widget.passport.bday.toLocal().toString().split(' ')[0]),
+            _buildDetailRow('Отец (ID)', widget.passport.father.toString()),
+            _buildDetailRow('Мать (ID)', widget.passport.mother.toString()),
             _buildDetailRow(
-                'Молоко (л)', passport.milk?.toString() ?? 'Не указано'),
-            _buildDetailRow('Жирность (%)', passport.fatness.toString()),
-            _buildDetailRow('Инбридинг (%)', passport.inbredding.toString()),
+                'Молоко (л)', widget.passport.milk?.toString() ?? 'Не указано'),
+            _buildDetailRow('Жирность (%)', widget.passport.fatness.toString()),
+            _buildDetailRow(
+                'Инбридинг (%)', widget.passport.inbredding.toString()),
             _buildDetailRow('Прирост веса (г)',
-                passport.weightGain?.toString() ?? 'Не указано'),
-            _buildDetailRow('Здоровье', passport.health.toString()),
-            _buildDetailRow(
-                'Фертильность', passport.fertility?.toString() ?? 'Не указано'),
-            _buildDetailRow('Ценность', passport.worth.toString()),
+                widget.passport.weightGain?.toString() ?? 'Не указано'),
+            _buildDetailRow('Здоровье', widget.passport.health.toString()),
+            _buildDetailRow('Фертильность',
+                widget.passport.fertility?.toString() ?? 'Не указано'),
+            _buildDetailRow('Ценность', widget.passport.worth.toString()),
           ],
         ),
       ),
@@ -122,6 +176,17 @@ class AnimalDetailsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        actions: [
+          IconButton(
+            onPressed: () {
+              _addMilkInfo(context);
+            },
+            icon: const Icon(Icons.water_drop_rounded),
+          ),
+        ],
+      ),
       body: SingleChildScrollView(
         child: Column(
           children: [
