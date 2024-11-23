a6f13b610cd6020569e2ad4e05dfe916c4c57cb4
Maria.Pelageycheva <m.v.pelageycheva@urfu.ru>
2024-11-23 10:46:06 +0500

    firebase

--- lib/features/super_duper_algorithm/attribute.dart
+++ lib/features/super_duper_algorithm/attribute.dart
@@ -1,6 +1,6 @@
 enum Attribute {
   milk('Удой', 0, 25, 'л/день'),
-  fatness('Упитанность', 0, 10, null),
+  fatness('Упитанность', 1, 9, null),
   inbredding('Коэффициент инбридинга (F)', 0, 1, null),
   weightGain('Прирост веса', 0, 1, 'кг/день'),
   health('Здоровье', 1, 10, null),
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
+import 'dart:io';
+
 import 'package:breed_plus/features/super_duper_algorithm/backend.dart';
-// import 'package:firebase_core/firebase_core.dart';
-// import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 
 import 'common/app.dart';
@@ -8,17 +10,16 @@ import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  // await Firebase.initializeApp(
-  //   options: DefaultFirebaseOptions.currentPlatform,
-  // );
-  // final notificationSettings =
-  //     await FirebaseMessaging.instance.requestPermission(provisional: true);
+  if (!Platform.isWindows) {
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
+    final notificationSettings =
+        await FirebaseMessaging.instance.requestPermission(provisional: true);
 
-  // final apnsToken = await FirebaseMessaging.instance.getToken();
-  // print(apnsToken);
-  // if (apnsToken != null) {
-  //   print(apnsToken);
-  // }
+    final apnsToken = await FirebaseMessaging.instance.getToken();
+    print(apnsToken);
+  }
   await Backend.init();
   runApp(const App());
 }
