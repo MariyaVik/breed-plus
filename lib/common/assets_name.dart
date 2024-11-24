f74b67fbe5e48c463d82499fe10d80eab670dbc9
Maria.Pelageycheva <m.v.pelageycheva@urfu.ru>
2024-11-24 10:40:32 +0500

    icons

--- lib/common/assets_name.dart
+++ lib/common/assets_name.dart
@@ -1,7 +1,7 @@
 abstract class AppIcons {
-  static const String bottomSearch = 'assets/icons/sun.svg';
+  static const String bottomSearch = 'assets/icons/search.svg';
   static const String bottomRecommendations = 'assets/icons/cloud.svg';
   static const String bottomChats = 'assets/icons/location.svg';
-  static const String bottomProfile = 'assets/icons/lightning.svg';
-  static const String bottomSettings = 'assets/icons/lightning.svg';
+  static const String bottomProfile = 'assets/icons/farm.svg';
+  static const String bottomSettings = 'assets/icons/settings.svg';
 }
