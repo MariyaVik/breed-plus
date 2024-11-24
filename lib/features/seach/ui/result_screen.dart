032409b0a20212adc6afaa3997b5fafa1e69cae7
Maria.Pelageycheva <m.v.pelageycheva@urfu.ru>
2024-11-24 15:56:11 +0500

    result done

--- lib/features/seach/ui/result_screen.dart
+++ lib/features/seach/ui/result_screen.dart
@@ -17,24 +17,180 @@ class ResultScreen extends StatelessWidget {
         title: Text('Потомство'),
       ),
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Expanded(
-                child: MyAnimalCard(
-                  passport: info[0] as Passport,
-                  buttonShow: false,
+                child: Column(
+                  children: [
+                    Text('Выбранная особь'),
+                    MyAnimalCard(
+                      passport: info[0] as Passport,
+                      buttonShow: false,
+                    ),
+                  ],
                 ),
               ),
               Expanded(
-                child: MyAnimalCard(
-                  passport: (info[1] as ReproductionResponse).passport,
-                  buttonShow: false,
+                child: Column(
+                  children: [
+                    Text('Кандидат в пару'),
+                    MyAnimalCard(
+                      passport: (info[1] as ReproductionResponse).passport,
+                      buttonShow: false,
+                    ),
+                  ],
                 ),
               ),
             ],
           ),
-          // Text()
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 12),
+            child: Text(
+              'Возможное потомство',
+              style: Theme.of(context).textTheme.titleMedium,
+            ),
+          ),
+          Row(
+            children: [
+              Expanded(
+                child: Text(
+                  '50%',
+                  style: Theme.of(context).textTheme.titleLarge,
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              Expanded(
+                  child: Card(
+                child: Padding(
+                  padding: EdgeInsets.all(8),
+                  child: Column(
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text('Удой'),
+                          Text('24'),
+                        ],
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text('Упитанность'),
+                          Text('3'),
+                        ],
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text('Здоровье'),
+                          Text('8'),
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+              )),
+            ],
+          ),
+          Row(
+            children: [
+              Expanded(
+                child: Text(
+                  '25%',
+                  style: Theme.of(context).textTheme.titleLarge,
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              Expanded(
+                  child: Card(
+                child: Padding(
+                  padding: EdgeInsets.all(8),
+                  child: Column(
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text('Удой'),
+                          Text('22'),
+                        ],
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text('Упитанность'),
+                          Text('4'),
+                        ],
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text('Здоровье'),
+                          Text('6'),
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+              )),
+            ],
+          ),
+          Row(
+            children: [
+              Expanded(
+                child: Text(
+                  '25%',
+                  style: Theme.of(context).textTheme.titleLarge,
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              Expanded(
+                  child: Card(
+                child: Padding(
+                  padding: EdgeInsets.all(8),
+                  child: Column(
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text('Удой'),
+                          Text('15'),
+                        ],
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text('Упитанность'),
+                          Text('2'),
+                        ],
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text('Здоровье'),
+                          Text('7'),
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+              )),
+            ],
+          ),
+          // Text((info[1] as ReproductionResponse).offspringTraits.toString()),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 16),
+            child: Center(
+                child: ElevatedButton(
+                    onPressed: () {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(content: Text('Функция в разработке')),
+                      );
+                    },
+                    child: Text('Сохранить результат'))),
+          ),
         ],
       ),
     );
